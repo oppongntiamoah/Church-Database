0d9267f4551b5142ac9240fdd050938ed28c87fa
Emmanuel Ntiamoah <ladamob47@gmail.com>
2020-12-07 15:06:42 +0000

    okay

--- apps/member/views.py
+++ apps/member/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, CreateView, ListView, UpdateView, DeleteView
 from .models import Member, Visitor
-
+from apps.finance.models import Contribution
 
 class MemberList(ListView):
     model = Member
@@ -11,6 +11,12 @@ class MemberList(ListView):
 class MemberDetailView(DetailView):
     model = Member
     template_name = "member/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contributions'] = Contribution.objects.all()
+        
+        return context
 
 
 class MemberCreateView(CreateView):
