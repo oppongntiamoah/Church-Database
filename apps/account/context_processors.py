566f0785fe026ee50b622acfe1f23625753774b8
Emmanuel Ntiamoah <ladamob47@gmail.com>
2020-12-10 14:24:32 +0000

    new dashboard

--- apps/account/context_processors.py
+++ apps/account/context_processors.py
@@ -3,7 +3,7 @@
 def config(request):
 
     return {
-        'church_name': 'LOve Kingdom Ministries',
+        'church_name': 'Love Kingdom Ministries',
         'short_desc': 'Download the Color Admin app for iPhone®, iPad®, and Android™. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         'short_name': 'LKM',
         'church_motto': 'We Love because He Loved Us.',
